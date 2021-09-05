7096b7b40c391b9005fcc95a37e1416624623812
Gandhi Selvaraj <gandhidhamayanthi@gmail.com>
2021-09-05 23:56:08 +0530

    update

--- assets/page/customer-ledger-details.js
+++ assets/page/customer-ledger-details.js
@@ -93,7 +93,7 @@ function displayCustomerListInit() {
     var room_no = url.searchParams.get("room_no");
     if (typeof(room_no) != 'undefined' && room_no) {
         var data = { "list_key": "get_ledger", "booking_no": booking_no, "room_no": room_no };
-        $(".bill-sample").attr('href', "/booking-print-sample.html?booking_no=" + booking_no + "&room_no" + room_no);
+        $(".bill-sample").attr('href', "/booking-print-sample.html?booking_no=" + booking_no + "&room_no=" + room_no);
     } else {
         var data = { "list_key": "get_ledger", "booking_no": booking_no };
         $(".bill-sample").attr('href', "/booking-print-sample.html?booking_no=" + booking_no);
